c2e6ae09ee053b4be5f6a858a125e022f554b4e3
eldraco <eldraco@gmail.com>
2019-03-29 09:25:52 +0200

    More things!

--- sapito.py
+++ sapito.py
@@ -9,7 +9,17 @@ import argparse
 import sys
 from datetime import datetime
 from scapy.all import *
+import re
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    IMPORTANT = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 def do(pkt):
     """
@@ -28,7 +38,9 @@ def do(pkt):
                 amount_of_questions = DNSlayer.qdcount
                 amount_of_additional_records = DNSlayer.arcount
                 amount_of_answers = DNSlayer.ancount
-                print('SrcMAC: {}, SrcIP: {}. Name: , #Questions: {}. #Additional Records {}. #Answers: {}'.format(shw, srcip, amount_of_questions, amount_of_additional_records, amount_of_answers))
+                # TODO: Find the Authoritative Nameservers
+
+                print('SrcMAC: {}, SrcIP: {}. #Questions: {}. #Additional Records {}. #Answers: {}'.format(shw, srcip, amount_of_questions, amount_of_additional_records, amount_of_answers))
                 question_name = DNSlayer.fields['qd']
 
                 # Process the questions
@@ -40,52 +52,61 @@ def do(pkt):
                 answers_name = DNSlayer.fields['an']
                 for pos in range(0,amount_of_answers):
                     # Do we have rdata?
-                    try:
+                    if type(answers_name) == DNSRR:
                         if type(answers_name.rdata) == bytes:
-                            print('\tAnswer record Type: {}. Rdata name: {}'.format(answers_name.name, answers_name.rdata.decode('utf-8')))
-                            try:
-                                details = answers_name.rdata.decode('utf-8').split(',')
-                                for data1 in details:
+                            print('\tAnswer Type: {}. Rdata name: {}'.format(answers_name.name, answers_name.rdata.decode('utf-8')))
+                            details = answers_name.rdata.decode('utf-8').split(',')
+                            for data1 in details:
+                                # The split works even if there is no = in the string. In that case the string is returned inside an array
+                                tuple = data1.split('=')
+                                if 'model' in tuple[0].lower():
+                                    if 'macbook' in tuple[1].lower():
+                                        print(bcolors.IMPORTANT + '\t\tThe model of the MacBook is {}'.format(tuple[1]) + bcolors.ENDC)
+                                    # Now process all the models of ipads we know. Data from https://www.theiphonewiki.com
+                                    elif 'J208AP' in tuple[1]:
+                                        print(bcolors.IMPORTANT + '\t\tThis is an iPad Pro (10.5-inch) (iPad7,4). Model Number: {}'.format(tuple[1]) + bcolors.ENDC)
+                                    elif 'J321AP' in tuple[1]:
+                                        print(bcolors.IMPORTANT + '\t\tThis is an iPad Pro (12.9-inch) (3rd generation) Wi-Fi + Cellular model. It has 4 GB RAM and is available with 64, 256 and 512 GB of storage. Its identifier is iPad8,7. Model Number: {}'.format(tuple[1]) + bcolors.ENDC)
+                                    elif 'J127AP' in tuple[1]:
+                                        print(bcolors.IMPORTANT + '\t\tThis is an iPad Pro (9.7-inch) (iPad6,3). Model Number: {}'.format(tuple[1]) + bcolors.ENDC)
+                                    elif 'J81AP' in tuple[1]:
+                                        print(bcolors.IMPORTANT + '\t\tThis is the iPad Air 2 (iPad5,3). Model Number: {}'.format(tuple[1]) + bcolors.ENDC)
+                                    elif 'J72bAP' in tuple[1]:
+                                        print(bcolors.IMPORTANT + '\t\tThis is the iPad (6th generation) (iPad7,6). Model Number: {}'.format(tuple[1]) + bcolors.ENDC)
+                                    elif 'J71bAP' in tuple[1]:
+                                        print(bcolors.IMPORTANT + '\t\tThis is the iPad (6th generation) (iPad7,5). Model Number: {}'.format(tuple[1]) + bcolors.ENDC)
+                                    elif 'J128AP' in tuple[1]:
+                                        print(bcolors.IMPORTANT + '\t\tThis is the iPad Pro (9.7-inch) (iPad6,4). Model Number: {}'.format(tuple[1]) + bcolors.ENDC)
+                                    elif 'J82AP' in tuple[1]:
+                                        print(bcolors.IMPORTANT + '\t\tThis is the iPad Air 2 (iPad5,4). Model Number: {}'.format(tuple[1]) + bcolors.ENDC)
+                                    elif 'J318AP' in tuple[1]:
+                                        print(bcolors.IMPORTANT + '\t\tThis is the iPad Pro (11-inch) Wi-Fi + Cellular model. It has 4 GB RAM and is available with 64, 256 and 512 GB of storage. Model Number: {}'.format(tuple[1]) + bcolors.ENDC)
+                                    elif 'J' in tuple[1] and 'AP' in tuple[1]:
+                                        print(bcolors.IMPORTANT + '\t\tAn Apple device that we don\'t know!!. Search it manually in https://www.theiphonewiki.com. Model Number: {}'.format(tuple[1]) + bcolors.ENDC)
+                                    else:
+                                        print('\t\tThe model of the device is {}'.format(tuple[1]))
+                                elif 'osxvers' in tuple[0].lower():
+                                    # Sometimes the str is broken wit 'ecolor' at the end and some 3 numbers
                                     try:
-                                        tuple = data1.split('=')
-                                        if 'model' in tuple[0].lower():
-                                            if 'macbook' in tuple[1].lower():
-                                                print('\t\tThe model of the MacBook {}'.format(tuple[1]))
-                                            else:
-                                                print('\t\tThe model of the device is {}'.format(tuple[1]))
-
-                                        elif 'osxvers' in tuple[0].lower():
-                                            # Sometimes the str is broken wit 'ecolor' at the end and some 3 numbers
-                                            try:
-                                                temp = tuple[1].split('ecolor')[0]
-                                            except TypeError:
-                                                temp = tuple[1]
-                                            print('\t\tThe osx version is {}'.format(temp))
-                                        elif 'Elmedia Video Player' in tuple[0] and 'airplay' in tuple[0]:
-                                            print('\t\tAirplay Enabled in this host.')
-
-                                        elif len(tuple) == 1:
-                                            # Its a text, but not formated with , or =. so just interpret it.
-                                            print('\t\tData to process: {}'.format(tuple))
-                                        else:
-                                            print('\t\tother data here?: {}'.format(tuple))
+                                        temp = tuple[1].split('ecolor')[0]
                                     except TypeError:
-                                        print('\tother error')
-                            except TypeError:
-                                print('\terror')
+                                        temp = tuple[1]
+                                    print(bcolors.IMPORTANT + '\t\tThe osx version is {}'.format(temp) + bcolors.ENDC)
+                                elif 'Elmedia Video Player' in tuple[0] and 'airplay' in tuple[0]:
+                                    print('\t\tAirplay Enabled in this host.')
+
+                                elif len(tuple) == 1:
+                                    # Its a text, but not formated with , or =. so just interpret it.
+                                    print('\t\tData to process: {}'.format(tuple[0]))
+                                else:
+                                    print('\t\tother data here?: {}'.format(tuple))
                         else:
-                            print('\tAnswer record Type: {}. Rdata name: {}'.format(answers_name.name, answers_name.rdata))
-                        answers_name = answers_name.payload
-                    except AttributeError:
-                        print('Error with an Answer. Doesn\'t have an rdata: {}'.format(answers_name))
-                        sys.exit(0)
-
-
-                # Find if the Apple is using the airdrop
-                # 	Answer record Type: DNS Resource Record. Rdata name: model=MacBookAir7,2osxvers=18
-                # 	Answer record Type: DNS Resource Record. Rdata name: 2deae6e5bd95._airdrop._tcp.local.
-
-
+                            print('\tAnswer Type: {}. Rdata name: {}'.format(answers_name.name, answers_name.rdata))
+                    elif type(answers_name) == DNSRRSRV:
+                        print('\tAnswer Type: {}. Target: {}. RRname: {}'.format(answers_name.name, answers_name.target.decode('utf-8'), answers_name.rrname.decode('utf-8')))
+                        print('\t\tData to process: {}'.format(answers_name.target.decode('utf-8')))
+                        print('\t\tData to process: {}'.format(answers_name.rrname.decode('utf-8')))
+                    answers_name = answers_name.payload
 
 
                 # Process the Additional records
@@ -93,24 +114,25 @@ def do(pkt):
                 additional_name = DNSlayer.fields['ar']
                 for pos in range(0,amount_of_additional_records):
                     if type(additional_name) == DNSRROPT:
-                        print('\tAdditional record Name: {}. Rdata name: {}'.format(additional_name.name, additional_name.rdata[0].name))
+                        print('\tAdditional Type: {}. Rdata name: {}'.format(additional_name.name, additional_name.rdata[0].name))
                     elif type(additional_name) == DNSRRNSEC:
-                        print('\tAdditional record Name: {}. RRName: {}'.format(additional_name.name, additional_name.rrname.decode('utf-8')))
+                        print('\tAdditional Type: {}. RRName: {}'.format(additional_name.name, additional_name.rrname.decode('utf-8')))
                         # This may be a DNS PTR style ipv6
                     elif type(additional_name) == DNSRR:
                         if type(additional_name.rdata) == bytes:
-                            print('\tAdditional record Name: {}. Rdata Bytes: {}'.format(additional_name.name, additional_name.rdata))
+                            print('\tAdditional Type: {}. Rdata Bytes: {}'.format(additional_name.name, additional_name.rdata))
+                            # Additional record Name: DNS Resource Record. Rdata Bytes: b'rpBA=2D:F6:28:9B:87:99rpAD=82ae95302041rpHI=85e0df6055a0rpHN=2d48a2585755rpVr=164.16rpHA=0736452b945f'
+                            rdata = additional_name.rdata.decode('utf-8')
                         elif type(additional_name.rdata) == dict:
-                            print('\tAdditional record Name: {}. Rdata Name: {}'.format(additional_name.name, additional_name.rdata[0].name))
+                            print('\tAdditional Type: {}. Rdata Name: {}'.format(additional_name.name, additional_name.rdata[0].name))
                     elif type(additional_name) == DNSRRSRV:
-                        print('DNSRRSRV')
+                        print('\tAdditional Type: {}. Target: {}. RRname: {}'.format(additional_name.name, additional_name.target.decode('utf-8'), additional_name.rrname.decode('utf-8')))
+                        print('\t\tData to process: {}'.format(additional_name.target.decode('utf-8')))
+                        print('\t\tData to process: {}'.format(additional_name.rrname.decode('utf-8')))
                     else:
                         # Probably NoneType
                         print('Attention! Other type: {}'.format(type(additional_name)))
                     additional_name = additional_name.payload
-                # Process the records
-                # Additional record Name: DNS Resource Record. Rdata Bytes: b'rpBA=2D:F6:28:9B:87:99rpAD=82ae95302041rpHI=85e0df6055a0rpHN=2d48a2585755rpVr=164.16rpHA=0736452b945f'
-                # Additional record Name: DNS Resource Record. Rdata Bytes: b'model=MacBookAir7,2osxvers=18'
 
 
 
