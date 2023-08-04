f9a8a4ce7e9a8c0f196d57dd9fa0f1fa368d6910
Veronica Valeros <vero.valeros@gmail.com>
2023-08-04 19:20:08 +0200

    Use f" for printing

--- sapito.py
+++ sapito.py
@@ -77,12 +77,12 @@ def do(pkt):
                 print(bcolors.HEADER + f' > Questions: \033[36m{num_questions}\033[95m' + bcolors.ENDC)
                 for pos in range(0,num_questions):
                     if args.debug:
-                        print(f"\t\t[Debug] Question Type: {question_name.qname}. Payload: {question_name.payload}")
+                        print(f"\t[Debug] Question Type: {question_name.qname}. Payload: {question_name.payload}")
                     try:
-                        print(f"\t\t{question_name.qname.decode('utf-8')}")
+                        print(f"\t{question_name.qname.decode('utf-8')}")
                     except AttributeError:
                         # Some versions of scapy do not give a Byte String
-                        print(f"\t\t{question_name}")
+                        print(f"\t{question_name}")
                     try:
                         question_name = question_name.payload
                     except AttributeError:
